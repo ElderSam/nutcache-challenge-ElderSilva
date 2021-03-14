fc1e20a1ead8cc84eee502cf20df16bd5d957fc0
ElderSam <eldersamuel98@gmail.com>
2021-03-14 12:30:09 -0300

    add Header

--- src/app/App.js
+++ src/app/App.js
@@ -1,11 +1,56 @@
+import React from 'react';
+import { makeStyles, CssBaseline, createMuiTheme, ThemeProvider } from '@material-ui/core';
+
+import Header from '../components/Header';
 import SideMenu from '../components/SideMenu';
-import './App.css';
+
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: "#333996",
+      light: '#3c44b126'
+    },
+    secondary: {
+      main: "#f83245",
+      light: '#f8324526'
+    },
+    background: {
+      default: "#f4f5fd"
+    },
+  },
+  overrides:{
+    MuiAppBar:{
+      root:{
+        transform:'translateZ(0)'
+      }
+    }
+  },
+  props:{
+    MuiIconButton:{
+      disableRipple:true
+    }
+  }
+})
+
+const useStyles = makeStyles({
+  appMain: {
+    paddingLeft: '320px',
+    width: '100%',
+  }
+})
 
 function App() {
+  const classes = useStyles();
+
   return (
-    <div className="App">
+    <ThemeProvider theme={theme}>
       <SideMenu />
-    </div>
+      <div className={classes.appMain}>
+      <Header />
+      </div>
+      <CssBaseline />
+    </ThemeProvider>
   );
 }
 
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,62 @@
-import React, { Component } from 'react';
-import './Header.css';
+import React from 'react'
+import { AppBar, Toolbar, Grid, InputBase, IconButton, Badge, makeStyles } from '@material-ui/core'
+import NotificationsNoneIcon from '@material-ui/icons/NotificationsNone';
+import ChatBubbleOutlineIcon from '@material-ui/icons/ChatBubbleOutline';
+import PowerSettingsNewIcon from '@material-ui/icons/PowerSettingsNew';
+import SearchIcon from '@material-ui/icons/Search';
 
-class Header extends Component {
-    render() {
-        return(
-            <header className="App-header">
-                <p>Employee app</p>
-            </header>
-        )
+
+const useStyles = makeStyles(theme => ({
+    root: {
+        backgroundColor: '#fff',
+    },
+    searchInput: {
+        opacity: '0.6',
+        padding: `0px ${theme.spacing(1)}px`,
+        fontSize: '0.8rem',
+        '&:hover': {
+            backgroundColor: '#f2f2f2'
+        },
+        '& .MuiSvgIcon-root': {
+            marginRight: theme.spacing(1)
+        }
     }
-}
+}))
 
-export default Header;
+export default function Header() {
+
+    const classes = useStyles();
+
+    return (
+        <AppBar position="static" className={classes.root}>
+            <Toolbar>
+                <Grid container
+                    alignItems="center">
+                    <Grid item>
+                        <InputBase
+                            placeholder="Search topics"
+                            className={classes.searchInput}
+                            startAdornment={<SearchIcon fontSize="small" />}
+                        />
+                    </Grid>
+                    <Grid item sm></Grid>
+                    <Grid item>
+                        <IconButton>
+                            <Badge badgeContent={4} color="secondary">
+                                <NotificationsNoneIcon fontSize="small" />
+                            </Badge>
+                        </IconButton>
+                        <IconButton>
+                            <Badge badgeContent={3} color="primary">
+                                <ChatBubbleOutlineIcon fontSize="small" />
+                            </Badge>
+                        </IconButton>
+                        <IconButton>
+                            <PowerSettingsNewIcon fontSize="small" />
+                        </IconButton>
+                    </Grid>
+                </Grid>
+            </Toolbar>
+        </AppBar>
+    )
+}
