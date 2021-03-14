82668f6ec601c6cc35f0335f9261c62a8322367b
ElderSam <eldersamuel98@gmail.com>
2021-03-14 09:30:06 -0300

    add SideMenu

--- src/app/App.js
+++ src/app/App.js
@@ -1,10 +1,10 @@
-import Header from './components/Header';
+import SideMenu from '../components/SideMenu';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <Header />
+      <SideMenu />
     </div>
   );
 }
